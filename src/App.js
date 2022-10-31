e02d2c69704209a3f15afa4d2705b2bbca348aee
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-10-31 20:49:00 +0300

    adding calendar strip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./components/header";
 import Nav from "./components/nav";
 import Slider from "./components/slider";
@@ -8,28 +7,30 @@ import News from "./components/news";
 import Form from "./components/form";
 import Block from "./components/blockFooter";
 import LastBlock from "./components/blockFooterLast";
+import Calendar from "./components/calendar";
 
 function App() {
-return (
+  return (
     <>
-        <Header/>
-        <Nav/>
-        <Slider/>
-        <main>
-            <section>s</section>
-            <section>
-                <News/>
-                <Form/>
-            </section>
-        </main>
-        <footer >
-            <Block/>
-            <hr/>
-            <LastBlock/>
-
-        </footer>
+      <Header />
+      <Nav />
+      <Slider />
+      <main>
+        <section>
+          <Calendar />
+        </section>
+        <section>
+          <News />
+          <Form />
+        </section>
+      </main>
+      <footer>
+        <Block />
+        <hr />
+        <LastBlock />
+      </footer>
     </>
-)
+  );
 }
 
 export default App;
---
+++ src/components/calendar/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import styles from "./calendar.module.scss";
+
+function DateBtn({ date, selected: _, free }) {
+  return (
+    <div className={styles.dateBtn + (free ? " " + styles.freeDateBtn : "")}>
+      {date}
+    </div>
+  );
+}
+
+export default function Calendar() {
+  const dates = [
+    { date: 1, selected: false, free: false },
+    { date: 2, selected: false, free: false },
+    { date: 3, selected: true, free: false },
+    { date: 4, selected: false, free: false },
+    { date: 5, selected: false, free: false },
+    { date: 6, selected: false, free: true },
+    { date: 7, selected: false, free: false },
+    { date: 8, selected: false, free: false },
+    { date: 9, selected: false, free: true },
+    { date: 10, selected: false, free: true },
+    { date: 11, selected: false, free: false },
+    { date: 12, selected: false, free: false },
+    { date: 13, selected: false, free: false },
+    { date: 14, selected: false, free: false },
+    { date: 15, selected: false, free: true },
+  ];
+
+  return (
+    <>
+      <div className={styles.datesStrip}>
+        {dates.map(({ date, selected, free }) => (
+          <DateBtn date={date} selected={selected} free={free} />
+        ))}
+      </div>
+    </>
+  );
+}
