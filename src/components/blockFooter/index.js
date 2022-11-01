d40a8f71b695d45c73773d10739c6bb2bfa6db74
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-01 21:51:07 +0300

    block footer links

--- src/components/blockFooter/index.js
+++ src/components/blockFooter/index.js
@@ -2,36 +2,48 @@ import React, { useRef, useState } from "react";
 
 import styles from "./block.module.scss";
 
-export default function Block(){
-    return(
-        <>
-            <div className={styles.block1}>
-                <div className={styles.group1}>
-                    <div className={styles.item1}>
-                        <h1>Касса</h1>
-                        <a href="tel:84959531317">+7 (495) 953-13-17</a>
-                    </div>
-                    <div className={styles.item2}>
-                        <p>Понедельник–Воскресенье 12:00– 19:30 <br/> Перерыв 15:00–16.00</p>
-                    </div>
+const SPONSORS = [
+  "http://персональныеданные.дети/",
+  "https://organizations.kultura.mos.ru/organizations/gbuk_gmoskvy_moskovskii_teatr_teatr_luny.html",
+  "https://bus.gov.ru/pub/info-card/137565?activeTab=3",
+  "http://ag.mos.ru/",
+  "http://hrculturemos.ru/bank/",
+  "https://historydepositarium.ru/",
+  "http://www.lunatheatre.ru/",
+  "https://grants.culture.ru/helpful_information/",
+  "http://www.lunatheatre.ru/pages/otkrytaya-informaciya",
+];
 
-                </div>
-                <div className={styles.group2}>
-                    <a href=""><img src="/img/sponsor/1.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/2.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/3.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/4.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/5.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/6.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/7.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/8.png" alt=""/></a>
-                    <a href=""><img src="/img/sponsor/9.png" alt=""/></a>
-                </div>
-                <div className={styles.group3}><img src="/img/footer_logo.png" alt=""/></div>
-            </div>
-            <div className={styles.block2}>
-                <div className={styles.group2}></div>
-            </div>
-        </>
-    )
+export default function Block() {
+  return (
+    <>
+      <div className={styles.block1}>
+        <div className={styles.group1}>
+          <div className={styles.item1}>
+            <h1>Касса</h1>
+            <a href="tel:84959531317">+7 (495) 953-13-17</a>
+          </div>
+          <div className={styles.item2}>
+            <p>
+              Понедельник–Воскресенье 12:00– 19:30 <br /> Перерыв 15:00–16.00
+            </p>
+          </div>
+        </div>
+        <div className={styles.group2}>
+          {SPONSORS.map((link, i) => (
+            <a href={link}>
+              <img src={`/img/sponsor/${i + 1}.png`} alt="" />
+            </a>
+          ))}
+        </div>
+        <div className={styles.group3}>
+          <img src="/img/footer_logo.png" alt="" />
+        </div>
+      </div>
+      <div className={styles.block2}>
+        <div className={styles.group2}></div>
+      </div>
+    </>
+  );
 }
+
