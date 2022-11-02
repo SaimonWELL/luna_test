013b1f6447cceb2960180150459ab53189e9a695
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-02 19:36:37 +0300

    changing date on calendar

--- src/App.js
+++ src/App.js
@@ -17,30 +17,30 @@ function App() {
   const [itemsNews, setItemsNews] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
-  const apiUrl2 = "http://theatre.restomatik.ru:1337/api/articles?sort[0]=publishedAt%3Adesc&pagination[pageSize]=5";
-  const apiUrl = "http://theatre.restomatik.ru:1337/api/shows?filters[date][$gt]=2022-10-19&sort[0]=date&populate=play.cover,play.director&pagination[pageSize]=4";
+  const apiUrl2 =
+    "http://theatre.restomatik.ru:1337/api/articles?sort[0]=publishedAt%3Adesc&pagination[pageSize]=5";
+  const apiUrl =
+    "http://theatre.restomatik.ru:1337/api/shows?filters[date][$gt]=2022-10-19&sort[0]=date&populate=play.cover,play.director&pagination[pageSize]=4";
   React.useEffect(() => {
     async function fetchData() {
-
       try {
         const [itemsResponse, newsResponse] = await Promise.all([
           axios.get(apiUrl),
-          axios.get(apiUrl2)
+          axios.get(apiUrl2),
         ]);
 
         setIsLoading(false);
 
         setItems(itemsResponse.data.data);
-        setItemsNews(newsResponse.data.data)
+        setItemsNews(newsResponse.data.data);
       } catch (error) {
-        alert('Ошибка при запросе данных!');
+        alert("Ошибка при запросе данных!");
         console.error(error);
       }
     }
 
     fetchData();
   }, []);
-
 
   return (
     <>
--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -2,48 +2,57 @@ import React from "react";
 
 import styles from "./calendar.module.scss";
 
-function DateBtn({ date, selected, free }) {
-  return selected ? (
-    <div className={styles.selectedCalendar}>
-      <img
-        className={styles.calendarLuna}
-        src="/img/calendar_luna.svg"
-        width="80px"
-        alt=""
-      />
-      <div className={styles.selectedDate}>{date}</div>
-    </div>
-  ) : (
-    <div className={styles.dateBtn + (free ? " " + styles.freeDateBtn : "")}>
-      {date}
-    </div>
-  );
-}
-
 export default function Calendar() {
   const dates = [
-    { date: 1, selected: false, free: false },
-    { date: 2, selected: false, free: false },
-    { date: 3, selected: true, free: false },
-    { date: 4, selected: false, free: false },
-    { date: 5, selected: false, free: false },
-    { date: 6, selected: false, free: true },
-    { date: 7, selected: false, free: false },
-    { date: 8, selected: false, free: false },
-    { date: 9, selected: false, free: true },
-    { date: 10, selected: false, free: true },
-    { date: 11, selected: false, free: false },
-    { date: 12, selected: false, free: false },
-    { date: 13, selected: false, free: false },
-    { date: 14, selected: false, free: false },
-    { date: 15, selected: false, free: true },
+    { date: 1, free: false },
+    { date: 2, free: false },
+    { date: 3, free: false },
+    { date: 4, free: false },
+    { date: 5, free: false },
+    { date: 6, free: true },
+    { date: 7, free: false },
+    { date: 8, free: false },
+    { date: 9, free: true },
+    { date: 10, free: true },
+    { date: 11, free: false },
+    { date: 12, free: false },
+    { date: 13, free: false },
+    { date: 14, free: false },
+    { date: 15, free: true },
   ];
+
+  const [selected, setSelected] = React.useState(3);
+
+  function DateBtn({ date, selected, free }) {
+    return selected ? (
+      <div className={styles.selectedCalendar}>
+        <img
+          className={styles.calendarLuna}
+          src="/img/calendar_luna.svg"
+          width="80px"
+          alt=""
+        />
+        <div className={styles.selectedDate}>{date}</div>
+      </div>
+    ) : (
+      <div
+        className={styles.dateBtn + (free ? " " + styles.freeDateBtn : "")}
+        onClick={() => {
+          if (!free) {
+            setSelected(date);
+          }
+        }}
+      >
+        {date}
+      </div>
+    );
+  }
 
   return (
     <>
       <div className={styles.datesStrip}>
-        {dates.map(({ date, selected, free }) => (
-          <DateBtn date={date} selected={selected} free={free} />
+        {dates.map(({ date, free }) => (
+          <DateBtn date={date} selected={selected === date} free={free} />
         ))}
       </div>
     </>
