831ec2517b6b088c19a58d73d04723544c51c0f9
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-02 17:57:11 +0300

    adding date to slider

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -14,6 +14,7 @@ import "./styles.css";
 import { Autoplay, EffectFade } from "swiper";
 
 export default function Slider({ items }) {
+  console.log(items);
   return (
     <>
       <Swiper
@@ -26,33 +27,71 @@ export default function Slider({ items }) {
         modules={[EffectFade, Autoplay]}
         className="mySwiper"
       >
-        {items.map(offer => (
-          <SwiperSlide key={offer.id} style={{ background: `url('http://theatre.restomatik.ru:1337${offer.attributes.play.data.attributes.cover.data.attributes.url}')no-repeat center`, backgroundSize: 'cover' }}>
+        {items.map((offer) => (
+          <SwiperSlide
+            key={offer.id}
+            style={{
+              background: `url('http://theatre.restomatik.ru:1337${offer.attributes.play.data.attributes.cover.data.attributes.url}')no-repeat center`,
+              backgroundSize: "cover",
+            }}
+          >
             <div className={styles.slide}>
               <div className={styles.wrapper}>
                 <div className={styles.block1}>
-                  <p className={styles.author}>{offer.attributes.play.data.attributes.director.data.attributes.fullname}</p>
-                  <p className={styles.title}>{offer.attributes.play.data.attributes.title}</p>
+                  <p className={styles.author}>
+                    {
+                      offer.attributes.play.data.attributes.director.data
+                        .attributes.fullname
+                    }
+                  </p>
+                  <p className={styles.title}>
+                    {offer.attributes.play.data.attributes.title}
+                  </p>
                   <div className={styles.premiere}>
-                    <a href={`${offer.webSite}`} target={"_blank"}>Премьера</a>
+                    <a href={`${offer.webSite}`} target={"_blank"}>
+                      Премьера
+                    </a>
                   </div>
                 </div>
                 <div className={styles.block2}>
-                  <p className={styles.date}>{offer.date}</p>
-                  <p className={styles.month}>{offer.month}</p>
+                  <p className={styles.date}>
+                    {offer.attributes.date_str.split(" ")[0]}
+                  </p>
+                  <p className={styles.month}>
+                    {offer.attributes.date_str.split(" ")[1]}
+                  </p>
                   <div className={styles.buy}>
-                    <a href={`${offer.attributes.tickets_link}`} target={"_blank"}>КУПИТЬ БИЛЕТ</a>
+                    <a
+                      href={`${offer.attributes.tickets_link}`}
+                      target={"_blank"}
+                    >
+                      КУПИТЬ БИЛЕТ
+                    </a>
                   </div>
                 </div>
               </div>
-              <div className={styles.startCalendar} style={{ background: `url('http://theatre.restomatik.ru:1337${offer.attributes.play.data.attributes.cover.data.attributes.url}')no-repeat center `, backgroundSize: 'cover' }}>
+              <div
+                className={styles.startCalendar}
+                style={{
+                  background: `url('http://theatre.restomatik.ru:1337${offer.attributes.play.data.attributes.cover.data.attributes.url}')no-repeat center `,
+                  backgroundSize: "cover",
+                }}
+              >
                 <div className={styles.overlay}></div>
               </div>
               <div className={styles.startCalendarText}>
                 <h1>ОКТЯБРЬ</h1>
                 <div className={styles.buttons}>
-                  <a href="http://www.lunatheatre.ru/afisha" target={"_blank"}><div className={styles.post}><p>Афиша</p></div></a>
-                  <a href="http://www.lunatheatre.ru/shows" target={"_blank"}><div className={styles.allPost}><p>Все спектакли</p></div></a>
+                  <a href="http://www.lunatheatre.ru/afisha" target={"_blank"}>
+                    <div className={styles.post}>
+                      <p>Афиша</p>
+                    </div>
+                  </a>
+                  <a href="http://www.lunatheatre.ru/shows" target={"_blank"}>
+                    <div className={styles.allPost}>
+                      <p>Все спектакли</p>
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
@@ -62,3 +101,4 @@ export default function Slider({ items }) {
     </>
   );
 }
+
