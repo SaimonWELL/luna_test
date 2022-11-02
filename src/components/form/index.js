b6406bff0d0ef11e13510dc2a27677248d5e25a2
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-02 23:46:48 +0300

    adding telegram icon

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,13 +2,22 @@ import React, { useRef, useState } from "react";
 
 import styles from "./form.module.scss";
 
+const TG_LINK = "https://t.me/lunatheatre";
+
 export default function Form() {
   return (
     <div className={styles.block_form}>
       <div className={styles.block}>
         <h2>Следи за жизнью театра в Telegram</h2>
         <form action="URL">
-          <button href="https://t.me/lunatheatre">подписаться</button>
+          <img
+            src="/img/tg.svg"
+            width="40px"
+            height="40px"
+            alt=""
+            onClick={() => window.open(TG_LINK)}
+          />
+          <button onClick={() => window.open(TG_LINK)}>Подписаться</button>
         </form>
       </div>
     </div>
