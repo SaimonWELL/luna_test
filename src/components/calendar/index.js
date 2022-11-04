7f095b0a4282a4795ac23e8aef289075eb835cc0
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-04 23:04:51 +0300

    moving calendar

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -4,30 +4,28 @@ import Item from "../../components/items";
 import styles from "./calendar.module.scss";
 
 export default function Calendar({ items }) {
-  const dates = Array.from({ length: 15 }, (_, i) => {
-    const date = new Date("2022-10-17");
-    date.setTime(date.getTime() + 1000 * 60 * 60 * 24 * i);
-    return {
-      date: date,
-      free: !items.some((item) => {
-        const itemdate = new Date(item.attributes.date);
-        return itemdate.getTime() === date.getTime();
-      }),
-    };
-  });
+  const [selected, setSelected] = React.useState(new Date("2022-10-1"));
+  const [firstDate, setFirstDate] = React.useState(
+    (() => {
+      const date = new Date("2022-10-17");
+      date.setTime(date.getTime() - 1000 * 60 * 60 * 24 * (date.getDate() - 1));
+      return date;
+    })()
+  );
 
-  const getFirstDate = () => {
-    for (let i = 0; i < dates.length; i++) {
-      const date = dates[i];
-      // console.log(date);
-      if (!date.free) {
-        return date.date;
-      }
-    }
-    return new Date("2022-10-17");
-  };
-
-  const [selected, setSelected] = React.useState(getFirstDate());
+  const dates = React.useMemo(() => {
+    return Array.from({ length: 15 }, (_, i) => {
+      const date = new Date();
+      date.setTime(firstDate.getTime() + 1000 * 60 * 60 * 24 * i);
+      return {
+        date: date,
+        free: !items.some((item) => {
+          const itemdate = new Date(item.attributes.date);
+          return itemdate.getTime() === date.getTime();
+        }),
+      };
+    });
+  }, [firstDate]);
 
   function DateBtn({ date, selected, free }) {
     const week = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
@@ -55,10 +53,28 @@ export default function Calendar({ items }) {
     );
   }
 
+  function moveDate(days) {
+    console.log(days);
+    setFirstDate((prev) => {
+      const d = new Date();
+      d.setTime(prev.getTime() + days * 1000 * 24 * 60 * 60);
+      return d;
+    });
+  }
+
   return (
     <>
+      <img className={styles.curLeft} src="/img/curtainsLeft.png" alt="" />
+      <img className={styles.curRight} src="/img/curtainsRight.png" alt="" />
       <div className={styles.datesStrip}>
-        <img className={styles.curLeft} src="/img/curtainsLeft.png" alt="" />
+        <img
+          src="/img/larr.png"
+          alt="<"
+          className={styles.larr}
+          onClick={() => {
+            moveDate(-2);
+          }}
+        />
         {dates.map(({ date, free }) => (
           <DateBtn
             key={date.getTime()}
@@ -67,7 +83,14 @@ export default function Calendar({ items }) {
             free={free}
           />
         ))}
-        <img className={styles.curRight} src="/img/curtainsRight.png" alt="" />
+        <img
+          src="/img/rarr.png"
+          alt=">"
+          className={styles.rarr}
+          onClick={() => {
+            moveDate(2);
+          }}
+        />
       </div>
       <div className={styles.cardsContainer}>
         {items
