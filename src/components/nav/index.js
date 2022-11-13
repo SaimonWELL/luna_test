3cf19d7b101a0b4e907b916cea2b81ce76b40e0b
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-13 19:40:08 +0300

    nav scaling fix

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -4,48 +4,60 @@ import styles from "./nav.module.scss";
 
 function Nav() {
   return (
-    <nav className={styles.menu}>
-      <ul className={styles.leftMenu}>
-        <li className={styles.menuItemL}>
+    <nav>
+      <ul>
+        <li>
           <a href="http://www.lunatheatre.ru/afisha">Афиша</a>
         </li>
-        <li className={styles.menuItemL}>
+        <li>
           <a href="http://www.lunatheatre.ru/shows">Спектакли</a>
         </li>
-        <li className={styles.menuItemL}>
+        <li>
           <a href="http://www.lunatheatre.ru/actors">труппа</a>
         </li>
-        <li className={styles.menuItemL}>
+        <li>
           <a href="http://www.lunatheatre.ru/pages/o-lune">театр</a>
         </li>
-        <li className={styles.menuItemL}>
+        <li>
           <a href="http://www.lunatheatre.ru/news">Новости</a>
         </li>
-        <li className={styles.menuItemL}>
+        <li>
           <a href="http://www.lunatheatre.ru/smi">Пресса</a>
         </li>
-        <li className={styles.menuItemL}>
+        <li>
           <a href="http://www.lunatheatre.ru/pages/kontakty">Контакты</a>
         </li>
       </ul>
-      <div className={styles.logo}>
-        <a href="#">
-          <img
-            className={styles.test}
-            src="/img/moon_logo.png"
-            alt=""
-            width={"124px"}
-            height={"124px"}
-          />
-          <img
-            className={styles.text_logo}
-            id={"href"}
-            src="/img/text_logo.png"
-            alt=""
-            width={"128px"}
-            height={"90px"}
-          />
-        </a>
+      <div className={styles.logoContainer}>
+        <img
+          className={styles.back_logo}
+          src="/img/back-logo.png"
+          width="450px"
+          height="350px"
+          alt=""
+        />
+        <img
+          className={styles.back_elipse}
+          src="/img/back_elipse.png"
+          width="230px"
+          height="230px"
+          alt=""
+        />
+        <img
+          className={styles.moon_logo}
+          src="/img/moon_logo.png"
+          alt=""
+          width="220px"
+          height="220px"
+        />
+        <img
+          className={styles.text_logo}
+          id={"href"}
+          src="/img/text_logo.png"
+          alt=""
+          width="250px"
+          height="150px"
+        />
       </div>
     </nav>
   );
