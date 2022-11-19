56fb365806e6cfebf6463d266527469d3caf12a0
demig00d <28487425+demig00d@users.noreply.github.com>
2022-11-19 09:05:49 +0300

    Other changes

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -2,38 +2,87 @@ import React from "react";
 
 import styles from "./news.module.scss";
 
-export default function News({ itemsNews }) {
-  // console.log(itemsNews);
+const API_URL = "http://theatre.restomatik.ru:1337";
+
+function cutToLength(s, l) {
+  const words = s.split(" ");
+  let i = 1;
+
+  console.log(words);
+
+  while (words.slice(0, i).join(" ").length < l) {
+    i += 1;
+
+    if (i > words.length) {
+      break;
+    }
+  }
+
+  const res = words.slice(0, i - 1).join(" ");
+
+  if (res.length < s.length) {
+    return res + "...";
+  } else {
+    return s;
+  }
+}
+
+export default function News({ itemsNews, setItemsNews }) {
+  console.log(itemsNews);
+  const news = React.useMemo(() => {
+    const res = itemsNews.map((item) => ({ item, style: styles.smallNews }));
+    if (res.length > 0) {
+      res[0].style = styles.bigNews;
+    }
+    return res;
+  }, [itemsNews]);
+
+  function scrollNews(d) {
+    setItemsNews((items) =>
+      Array.from(
+        { length: items.length },
+        (_, i) => items[(i + d + items.length) % items.length]
+      )
+    );
+  }
+
   return (
     <>
-      <div className={styles.header} >
-        <div className={styles.title}>
-          <p>НОВОСТИ ТЕАТРА</p>
-        </div>
-        <a href="http://www.lunatheatre.ru/news" target={"_blank"}>
-          <div className={styles.btn}>
-            <p>Читать все</p>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            <p>НОВОСТИ ТЕАТРА</p>
           </div>
-        </a>
-      </div>
-      <div className={styles.newsContent}>
-        <div className={styles.image}>
-          <img src="/img/news_bg.png" alt="" />
-        </div>
-        <div className={styles.news}>
-          {itemsNews.map((item) => (
-            <div className={styles.news_item} key={item.id}>
-              <p className={styles.item_title}>
-                <a
-                  className={styles.newslink}
-                  href={`http://www.lunatheatre.ru/news/${item.id}`}
-                >
-                  {item.attributes.title}
-                </a>
-              </p>
-              <p className={styles.newsDate}>{item.attributes.date_str}</p>
+          <a href="http://www.lunatheatre.ru/news">
+            <div className={styles.btn}>
+              <p>ЧИТАТЬ ВСЕ</p>
             </div>
+          </a>
+        </div>
+        <div className={styles.newsContent}>
+          {news.map(({ item, style }, i) => (
+            <a href={`${API_URL}/news/${item.id}`} key={i} className={style}>
+              <img
+                src={API_URL + item.attributes.cover.data.attributes.url}
+                alt=""
+              />
+              <div className={styles.date}>{item.attributes.date_str}</div>
+              <div className={styles.title}>
+                {i === 0
+                  ? item.attributes.title
+                  : cutToLength(item.attributes.title, 40)}
+              </div>
+              {i === 0 ? (
+                <div className={styles.text}>
+                  {cutToLength(item.attributes.text, 120)}
+                </div>
+              ) : null}
+            </a>
           ))}
+        </div>
+        <div className={styles.arrowsContainer}>
+          <img src="/img/newsLarr.png" alt="" onClick={() => scrollNews(-1)} />
+          <img src="/img/newsRarr.png" alt="" onClick={() => scrollNews(1)} />
         </div>
       </div>
     </>
